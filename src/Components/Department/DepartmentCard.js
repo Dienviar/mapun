66246845853db29b670d64685a16bce61b5ac6cf
Denver Danniel <dnvrdnnl09@gmail.com>
2022-04-26 10:46:53 +0800

    department complete

--- src/Components/Department/DepartmentCard.js
+++ src/Components/Department/DepartmentCard.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const DepartmentCard = () => {
+const DepartmentCard = ({title, icon}) => {
   return (
-    <div>DepartmentCard</div>
+    <div className='relative m-5 border-2 rounded-xl w-250px h-300px bg-blue opacity-80 screen600px:w-full'>
+        <div className='flex items-center justify-center w-full h-20'>
+            {icon}
+        </div>
+
+        <div className='flex items-center justify-center w-full h-auto px-3'>
+            <p className='text-xl font-bold text-center text-white'>{title}</p>
+        </div>
+
+        <div className='absolute w-full h-auto px-3 mt-2 text-center bottom-2'>
+            <p className='text-xs text-white'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+
+            <p className='mt-10 text-sm text-white'>
+                thisoffice@mapun.ph
+            </p>
+
+            <p className='text-sm text-white'>
+                +63 068 123 4567
+            </p>
+        </div>
+    </div>
   )
 }
 
--- src/Components/Department/DepartmentList.js
+++ src/Components/Department/DepartmentList.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import DepartmentCard from './DepartmentCard'
+import { GiFamilyHouse } from 'react-icons/gi'
+import { FaWallet } from 'react-icons/fa'
+import { BsFillHouseDoorFill } from 'react-icons/bs'
+import { MdPedalBike } from 'react-icons/md'
+import { AiFillWarning } from 'react-icons/ai'
+ 
 const DepartmentList = () => {
   return (
-    <div>DepartmentList</div>
+    <section className='flex justify-center w-full h-full mt-32'>
+        <article className='flex flex-wrap justify-center h-auto w-1000px'>
+            <DepartmentCard icon={<GiFamilyHouse className='w-10 h-10 text-white'/>} title={"Mayor's Office"}/>
+            <DepartmentCard icon={<GiFamilyHouse className='w-10 h-10 text-white'/>} title={"Office of the Vice Mayor's"}/>
+            <DepartmentCard icon={<GiFamilyHouse className='w-10 h-10 text-white'/>} title={"Office of the Sangguniang Bayan"}/>
+        
+            <DepartmentCard icon={<FaWallet className='w-10 h-10 text-white'/>} title={"Finance Department"}/>
+            <DepartmentCard icon={<FaWallet className='w-10 h-10 text-white'/>} title={"Office of the Municipal Treasurer"}/>
+            <DepartmentCard icon={<FaWallet className='w-10 h-10 text-white'/>} title={"Office of the Municipal Budget Officer"}/>
+            <DepartmentCard icon={<FaWallet className='w-10 h-10 text-white'/>} title={"Mayor's Office"}/>
+        
+            <DepartmentCard icon={<BsFillHouseDoorFill className='w-10 h-10 text-white'/>} title={"Housing and Land Department"}/>
+            <DepartmentCard icon={<BsFillHouseDoorFill className='w-10 h-10 text-white'/>} title={"Municipal Planning and Development Coordinator"}/>
+            <DepartmentCard icon={<BsFillHouseDoorFill className='w-10 h-10 text-white'/>} title={"Assessors Office"}/>
+            <DepartmentCard icon={<BsFillHouseDoorFill className='w-10 h-10 text-white'/>} title={"Municipal Engineer"}/>
+
+            <DepartmentCard icon={<MdPedalBike className='w-10 h-10 text-white'/>} title={"Tourism and Travel Department"}/>
+            <DepartmentCard icon={<MdPedalBike className='w-10 h-10 text-white'/>} title={"Municipal Tourism Office"}/>
+  
+            <DepartmentCard icon={<AiFillWarning className='w-10 h-10 text-white'/>} title={"Risk Reduction Department"}/>
+            <DepartmentCard icon={<AiFillWarning className='w-10 h-10 text-white'/>} title={"Municipal Disaster Risk Reduction  Management Office"}/>
+        </article>
+    </section>
   )
 }
 
