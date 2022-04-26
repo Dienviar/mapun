2bc1e6d28252e24624739a0fa1dfa1babdc4f4ac
Denver Danniel <dnvrdnnl09@gmail.com>
2022-04-26 10:00:54 +0800

    added icons

--- src/Components/Services/OnlineServicesCard.js
+++ src/Components/Services/OnlineServicesCard.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { FaPenNib } from 'react-icons/fa'
 
 const OnlineServicesCard = ({title, description}) => {
   return (
-    <div className='w-300px h-28 bg-blue flex opacity-90 rounded-2xl shadow-xl m-2 screen700px:w-full cursor-default'>
-        <div className='w-20 h-full flex justify-center items-center mx-2'>
-            <div className='w-16 h-16 bg-white rounded-full'></div>
+    <div className='flex m-2 shadow-xl cursor-default w-300px h-28 bg-blue opacity-90 rounded-2xl screen700px:w-full'>
+        <div className='flex items-center justify-center w-20 h-full mx-2'>
+            <div className='flex items-center justify-center w-16 h-16 rounded-full'>
+              <FaPenNib className='w-10 h-10 text-white'/>
+            </div>
         </div>
 
-        <div className='w-auto h-full flex items-center mx-2'>   
+        <div className='flex items-center w-auto h-full mx-2'>   
             <div>
-                <p className='text-white font-bold text-xl'>{title}</p>
-                <p className='text-white text-sm'>{description}</p>
+                <p className='text-xl font-bold text-white'>{title}</p>
+                <p className='text-sm text-white'>{description}</p>
             </div>
         </div>
     </div>
