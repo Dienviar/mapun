d9b36b628314c6b065a5448c098de40e803a84c5
Denver Danniel <dnvrdnnl09@gmail.com>
2022-04-26 10:10:57 +0800

    department page setup

---
+++ src/Pages/Department.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+const Department = () => {
+
+    document.title = "Department"
+    
+    useEffect(() => {
+      window.scrollTo(0, 0)
+    }, [])
+
+  return (
+    <main className='relative w-full h-auto'>
+
+    </main>
+  )
+}
+
+export default Department
